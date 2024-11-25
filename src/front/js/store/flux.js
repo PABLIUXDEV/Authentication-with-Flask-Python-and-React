96de6037395fd04246b69da1a1f60bc8d97157b3
Pablo Dávila <pablo.h.davila@gmail.com>
2024-11-25 22:28:09 +0000

    cambié ruta de servidor por variable de env

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,100 +1,102 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			token: null,
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      token: null,
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-			syncTokenFromSessionStore: () => {
-				const token	= sessionStorage.getItem("token");
-				console.log("Aplication just loaded, synching the session storage token");
-				if (token && token !="" && token != undefined) setStore({ token: token });
-			},
+      syncTokenFromSessionStore: () => {
+        const token = sessionStorage.getItem("token");
+        console.log(
+          "Aplication just loaded, synching the session storage token"
+        );
+        if (token && token != "" && token != undefined)
+          setStore({ token: token });
+      },
 
-			logout: () => {
-				sessionStorage.removeItem("token");
-				console.log("Login out");
-				setStore({ token: null });
-			},
+      logout: () => {
+        sessionStorage.removeItem("token");
+        console.log("Login out");
+        setStore({ token: null });
+      },
 
-			login: async (email, password) => {
-				const opts = {
-					method: 'POST',
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-						email: email,
-						password: password
-					})
-				};
-				
-				try{
-					const resp = await fetch("https://3001-pabliuxdev-authenticati-rjn5hjpgypq.ws-us116.gitpod.io/api/token", opts);
-					if (resp.status !== 200) {
-						alert("There has been some error");
-						return false;
-					} 
-	
-	
-					const data = await resp.json();
-					console.log("this came from the backend", data);
-					sessionStorage.setItem("token", data.access_token);
-					setStore({ token: data.access_token});
-					return true;
-				}
-				catch(error){
-					console.error("There has been an error login in")
-				}
-				},
+      login: async (email, password) => {
+        const opts = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        };
 
+        try {
+          const resp = await fetch(`${process.env.BACKEND_URL}api/token`, opts);
+          if (resp.status !== 200) {
+            alert("There has been some error");
+            return false;
+          }
 
-					getMessage: () => {
-						const store = getStore();
-						const opts = {
-							headers: {
-								"Authorization": "Bearer" + store.token
-							}
-						}
-							// fetching data from the backend
-						fetch("https://3001-pabliuxdev-authenticati-rjn5hjpgypq.ws-us116.gitpod.io/api/hello", opts)
-							.then(resp => resp.json())
-							.then(data => setStore({ message: data.message }))
-							.catch(error => console.log("Error loading message from backend", error))
-					},
-					changeColor: (index, color) => {
-						//get the store
-						const store = getStore();
+          const data = await resp.json();
+          console.log("this came from the backend", data);
+          sessionStorage.setItem("token", data.access_token);
+          setStore({ token: data.access_token });
+          return true;
+        } catch (error) {
+          console.error("There has been an error login in");
+        }
+      },
 
-						//we have to loop the entire demo array to look for the respective index
-						//and change its color
-						const demo = store.demo.map((elm, i) => {
-							if (i === index) elm.background = color;
-							return elm;
-						});
+      getMessage: () => {
+        const store = getStore();
+        const opts = {
+          headers: {
+            Authorization: "Bearer" + store.token,
+          },
+        };
+        // fetching data from the backend
+        fetch(`${process.env.BACKEND_URL}api/hello`, opts)
+          .then((resp) => resp.json())
+          .then((data) => setStore({ message: data.message }))
+          .catch((error) =>
+            console.log("Error loading message from backend", error)
+          );
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-						//reset the global store
-						setStore({ demo: demo });
-					}
-		}
-	};
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
